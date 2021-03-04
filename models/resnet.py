7b05a743b5b4b417f7329ef2c120c542d4d3b625
Bob Bell <bwbelljr@gmail.com>
2021-03-04 23:32:14 +0100

    fix: use resnet50 pretrained model

--- models/resnet.py
+++ models/resnet.py
@@ -7,6 +7,6 @@ import torchvision.models as models
 
 
 def resnet50():
-    backbone = models.__dict__['resnet50']()
+    backbone = models.__dict__['resnet50'](pretrained=True)
     backbone.fc = nn.Identity()
     return {'backbone': backbone, 'dim': 2048}
